cc3a405fa40a4635185d791c3e8802c706d9c5a9
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-21 17:49:50 -0300

    placeholder test

--- src/validators/auth.test.js
+++ src/validators/auth.test.js
@@ -1,1 +1,9 @@
+const { Factory, factoryNames } = require('../tests/factories');
 const validateAuth = require('./auth');
+
+describe('test', () => {
+  it('passes', () => {
+    const authObject = Factory.build(factoryNames.authObject);
+    expect(validateAuth(authObject)).toBeTruthy();
+  });
+});
