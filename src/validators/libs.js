37e78384ebf86ba18ec29b7e9dfdc7054b7f9300
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-21 12:37:48 -0300

    refactor: move executor to its own package

--- src/validators/libs.js
+++ src/validators/libs.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-console */
 
-// TODO: For now we're asuming that all libs are being passed to the watcher
-// We should change @notify-watcher/core to allow the creation of custom execute
-// functions that pass more libs, and then do the validation against those.
-// This also means that @notify-watcher/core should have a way to obtain the list
-// of libs that are being passed to watchers
-
+/**
+ * Validate that all the libs required by a watcher are
+ * included in the current watcher executor
+ * @param {Object} executor A @notify-watcher/executor instance
+ * @param {Array} libs The libs of the watcher being validated
+ * @param {String} watcherName The name of the watcher being validated
+ */
 function validateLibs(executor, libs, watcherName) {
   const missingLibs = [];
   for (let i = 0; i < libs.length; i += 1) {
