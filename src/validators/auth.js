3327c5cc3a0ba66d4dc2c38583f19874108d0c7b
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-21 18:23:18 -0300

    auth config validation

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -1,13 +1,66 @@
+/* eslint-disable no-console */
+const { AUTH_KEYS, AUTH_TYPE_KEYS } = require('../constants');
+
+const requiredItemKeys = Object.keys(AUTH_KEYS);
+const validTypeKeys = Object.keys(AUTH_TYPE_KEYS);
+
 const defaultOptions = {
   verbose: true,
 };
 
-function validateAuth(auth, options = {}) {
+function validateAuth(auth, watcherName, options = {}) {
   const { verbose } = { ...defaultOptions, ...options };
   if (!auth) return true;
-  // TODO: Here we should check the auth object for a valid auth specification
-  // this means, config.auth should contain all required fields for
-  // running the authenticated watcher
+
+  const invalidItems = [];
+  const authItemsKeys = Object.keys(auth);
+  if (authItemsKeys.length === 0) {
+    if (verbose)
+      console.warn(`WARN: Empty auth config for watcher ${watcherName}`);
+    return false;
+  }
+
+  for (let i = 0; i < authItemsKeys.length; i += 1) {
+    const authItemKey = authItemsKeys[i];
+    const authItem = auth[authItemKey];
+
+    const missingKeys = requiredItemKeys.filter(key => !authItem[key]);
+    if (missingKeys.length > 0 && verbose)
+      console.warn(
+        `WARN: Missing auth item keys ${missingKeys} for item ${authItemKey} for watcher ${watcherName}`,
+      );
+
+    const invalidKeys = Object.keys(authItem).filter(key => !AUTH_KEYS[key]);
+    if (invalidKeys.length > 0 && verbose)
+      console.warn(
+        `WARN: Invalid auth item keys ${invalidKeys} for item ${authItemKey} for watcher ${watcherName}`,
+      );
+
+    const authItemType = authItem[AUTH_KEYS.type];
+    const invalidType = authItemType && !validTypeKeys.includes(authItemType);
+    if (invalidType && verbose)
+      console.warn(
+        `WARN: Invalid auth item type ${authItemType} for item ${authItemKey} for watcher ${watcherName}`,
+      );
+
+    const description = authItemsKeys[AUTH_KEYS.description];
+    const emptyDescription = description && description.length === 0;
+    if (emptyDescription && verbose)
+      console.warn(
+        `WARN: Empty description for item ${authItemKey} for watcher ${watcherName}`,
+      );
+
+    const isInvalidItem =
+      missingKeys.length > 0 ||
+      invalidKeys.length > 0 ||
+      invalidType ||
+      emptyDescription;
+
+    if (isInvalidItem) invalidItems.push(authItem);
+  }
+
+  if (invalidItems.length > 0) return false;
+
   return true;
 }
 
--- src/validators/auth.test.js
+++ src/validators/auth.test.js
@@ -1,9 +1,72 @@
 const { Factory, factoryNames } = require('../tests/factories');
-const validateAuth = require('./auth');
+const { AUTH_TYPE_KEYS } = require('../constants');
+const originalValidateAuth = require('./auth');
 
-describe('test', () => {
-  it('passes', () => {
-    const authObject = Factory.build(factoryNames.authObject);
-    expect(validateAuth(authObject)).toBeTruthy();
+function validateAuth(auth) {
+  return originalValidateAuth(auth, 'testWatcherName', { verbose: false });
+}
+
+describe('validateAuth', () => {
+  describe('when auth is undefined', () => {
+    it('is valid', () => {
+      expect(validateAuth(undefined)).toBeTruthy();
+    });
+  });
+
+  describe('when auth is false', () => {
+    it('is valid', () => {
+      expect(validateAuth(false)).toBeTruthy();
+    });
+  });
+
+  describe('when auth is empty', () => {
+    it('is not valid', () => {
+      expect(validateAuth({})).toBeFalsy();
+    });
+  });
+
+  describe('when auth keys have type and description', () => {
+    it('is valid', () => {
+      const authObject = Factory.build(factoryNames.authObject);
+      expect(validateAuth(authObject)).toBeTruthy();
+    });
+  });
+
+  describe('when auth item does not have a description', () => {
+    it('is not valid', () => {
+      const authObject = Factory.build(factoryNames.authObject, {
+        secret2: { type: AUTH_TYPE_KEYS.string },
+      });
+      expect(validateAuth(authObject)).toBeFalsy();
+    });
+  });
+
+  describe('when auth item has an empty description', () => {
+    it('is not valid', () => {
+      const authObject = Factory.build(factoryNames.authObject, {
+        secret2: Factory.build(factoryNames.authItem, { description: '' }),
+      });
+      expect(validateAuth(authObject)).toBeFalsy();
+    });
+  });
+
+  describe('when auth item does not have a type', () => {
+    it('is not valid', () => {
+      const authObject = Factory.build(factoryNames.authObject, {
+        secret2: { description: 'a secret' },
+      });
+      expect(validateAuth(authObject)).toBeFalsy();
+    });
+  });
+
+  describe('when auth item has an invalid key', () => {
+    it('is not valid', () => {
+      const authObject = Factory.build(factoryNames.authObject, {
+        secret2: Factory.build(factoryNames.authItem, {
+          invalidKey: 'invalid',
+        }),
+      });
+      expect(validateAuth(authObject)).toBeFalsy();
+    });
   });
 });
